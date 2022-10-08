5a115549814fbe98b3b69fb09b716bf28cf74d6b
HinamiDesu <112235450+HinamiDesu@users.noreply.github.com>
2022-10-08 10:51:51 +0700

    Add files via upload

---
+++ src/pages/Abilities.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+   <Navbar />
+    <!---article-->
+    <main class="content margin-auto">
+        <article class="artical">
+            <section class="text-center title">
+                <h1>Abilities</h1>
+            </section>
+             
+            <section class="row-50 bg-white left">
+                <table class="table-cute">
+                    <tr>
+                        <td>HTML</td>
+                        <!--Emoji Windows: WIN+. or WIN+; | MAC: ^+Command+space-->
+                        <td>🌱🌱</td>
+                    </tr>
+                    <tr>
+                        <td>Canva</td>
+                        <td>🌱🌱🌱🌱</td>
+                    </tr>
+                    <tr>
+                        <td>English Communication</td>
+                        <td>🌱🌱🌱🌱</td>
+                    </tr>      
+                    <tr>
+                        <td>Photography</td>
+                        <td>🌱🌱🌱</td>
+                    </tr>    
+                </table> 
+            </section>
+            <section class="row-50 bg-red left">
+                <table class="table-cute">
+                    <tr>
+                        <td>Drawing</td>
+                        <td>🌱🌱🌱</td>
+                    </tr>
+                    <tr>
+                        <td>Singing</td>
+                        <td>🌱🌱🌱</td>
+                    </tr>
+                    <tr>
+                        <td>IMovie</td>
+                        <td>🌱🌱🌱</td>
+                    </tr>      
+                    <tr>
+                        <td>Computer Graphics</td>
+                        <td>🌱🌱🌱</td>
+                    </tr>    
+                </table>
+            </section>
+        </article>
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+    components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Experiences.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Navbar />
+    <footer class="content margin-auto">
+        <article style="color:aliceblue" class="text-center">
+            <section>
+                <span>"{{motto2}}"</span>
+            </section>
+            <section>
+                <span>--- {{author}} ---</span>
+            </section> 
+            <hr>
+            <div v-for="e in experiences" :key="e.title">
+                <section class="title" >
+                <span>{{e.title}}</span>
+            </section> 
+            
+            <article class="skill-menu margin-auto">
+                <ul class="ul-navbar">
+                    <li class="menu-list">
+                        <a href="index.html">{{e.subtitle}}</a>
+                    </li>
+                    <li class="menu-list">
+                        <a href="Experiences.html">{{e.subtitle2}}</a>
+                    </li>
+                </ul>
+            </article>
+            </div>
+        </article>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+    components: { Navbar },
+    data() {
+        return {
+            motto2 : 'Life Is The Art Of Drawing Without And Eraser',
+            author : 'John W Gardner',
+            experiences :[{
+                title : 'Scholarship',
+                subtitle : 'ทุนเพชรในชัยพฤกษ์ 100%',
+                subtitle2 : 'ทุนสหกรณ์ ประจำปี2565',
+            },{
+                title : 'Certificate',
+                subtitle : 'BU Creative SandBox X Workpoint 2022',
+                subtitle2 : 'การเเข่งขันประกวดร้านค้ากาชาดออนไลน์',
+            }
+
+            ]
+        }
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
